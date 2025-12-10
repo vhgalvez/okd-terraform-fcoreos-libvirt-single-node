aa6713164b041ec842e1c3cb0c0340ba5b91a849
Victor Galvez <vhgalvez@gmail.com>
2025-12-10 17:18:11 +0100

    update

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,9 +1,7 @@
 # terraform\terraform.tfvars
-
 ###############################################################
 # OKD SNO – Parámetros correctos y limpios
 ###############################################################
-
 
 # Red NAT aislada para SNO
 network_name = "okd-sno-net"
@@ -20,7 +18,9 @@ ssh_keys = [
   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdfUJjRAJuFcdO0J8CIOkjaKpqP6h9TqDRhZOJTac0199gFUvAJF9R/MAqwDLi2QI6OtYjz1CiCSVLtVQ2fTTIdwVibr+ZKDcbx/E7ivKUUbcmAOU8NP1gv3e3anoUd5k/0h0krP88CXosr41eTih4EcKhBAKbeZ11M0i9GZOux+/WweLtSQ3NU07sUkf1jDIoBungg77unmadqP3m9PUdkFP7tZ2lufcs3iq+vq8JaUBs/hZKNmWOXpnAyNxD9RlBJmvW2QgHmX53y3WC9bWUEUrwfDMB2wAqWPEDfj+5jsXQZcVE4pqD6T1cPaITnr9KFGnCCG1VQg31t1Jttg8z vhgalvez@gmail.com"
 ]
 
-
+###############################################################
+# Nodo Infra (DNS + NTP)
+###############################################################
 infra = {
   hostname = "infra.okd.local"
   ip       = "10.66.0.11"
@@ -29,6 +29,9 @@ infra = {
   memory   = 2048
 }
 
+###############################################################
+# Nodo SNO – Single Node OpenShift
+###############################################################
 sno = {
   hostname = "sno.okd.local"
   ip       = "10.66.0.10"
@@ -37,10 +40,16 @@ sno = {
   memory   = 32768
 }
 
-dns1    = "10.66.0.11" # Infra como DNS
+###############################################################
+# DNS + Gateway
+###############################################################
+dns1    = "10.66.0.11"  # Infra corre CoreDNS
 dns2    = "8.8.8.8"
 gateway = "10.66.0.1"
 
+###############################################################
+# Cluster Info
+###############################################################
 cluster_name   = "sno"
 cluster_domain = "okd.local"
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,45 +1,51 @@
-##############################################################
+# terraform/variables.tf
+###############################################################
 # VARIABLES GLOBALES PARA OKD SNO – LIBVIRT
-##############################################################
+###############################################################
+
+#############################
+# Red Libvirt
+#############################
+
+variable "network_name" {
+  type        = string
+  description = "Nombre de la red NAT en libvirt que usará OKD SNO"
+}
+
+variable "network_cidr" {
+  type        = string
+  description = "CIDR de la red para el clúster (ej: 10.66.0.0/24)"
+}
+
+#############################
+# Imágenes
+#############################
+
+variable "coreos_image" {
+  type        = string
+  description = "Ruta al QCOW2 de Fedora CoreOS"
+}
 
 variable "almalinux_image" {
   type        = string
   description = "Ruta al QCOW2 de AlmaLinux para el nodo infra"
 }
 
-variable "dns1" {
-  type        = string
-  description = "DNS primario a usar en la red"
-}
-
-variable "dns2" {
-  type        = string
-  description = "DNS secundario"
-}
-
-variable "gateway" {
-  type        = string
-  description = "Gateway de la red NAT"
-}
-
-variable "cluster_name" {
-  type        = string
-}
-
-variable "cluster_domain" {
-  type        = string
-}
-
-variable "timezone" {
-  type        = string
-}
+#############################
+# SSH Keys
+#############################
 
 variable "ssh_keys" {
   type        = list(string)
+  description = "Llaves SSH autorizadas"
 }
 
+#############################
+# Nodo Infra (DNS + NTP)
+#############################
+
 variable "infra" {
-  description = "Infra node (DNS + NTP)"
+  description = "Nodo infra que provee DNS + NTP"
   type = object({
     hostname = string
     ip       = string
@@ -47,4 +53,61 @@ variable "infra" {
     cpus     = number
     memory   = number
   })
+}
+
+#############################
+# SNO – Single Node OpenShift
+#############################
+
+variable "sno" {
+  description = "Nodo único del cluster OKD SNO"
+  type = object({
+    hostname = string
+    ip       = string
+    mac      = string
+    cpus     = number
+    memory   = number
+  })
+}
+
+#############################
+# DNS + Gateway
+#############################
+
+variable "dns1" {
+  type        = string
+  description = "Servidor DNS primario"
+}
+
+variable "dns2" {
+  type        = string
+  description = "Servidor DNS secundario"
+}
+
+variable "gateway" {
+  type        = string
+  description = "Puerta de enlace del cluster"
+}
+
+#############################
+# Cluster Name / Domain
+#############################
+
+variable "cluster_name" {
+  type        = string
+  description = "Nombre del cluster (sno)"
+}
+
+variable "cluster_domain" {
+  type        = string
+  description = "Dominio del cluster (okd.local)"
+}
+
+#############################
+# Timezone
+#############################
+
+variable "timezone" {
+  type        = string
+  description = "Zona horaria"
 }
