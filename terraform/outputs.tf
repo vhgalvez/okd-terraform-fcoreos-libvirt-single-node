0f208a9a9681ea4e0642cce3097e2615def1d2de
Victor Galvez <vhgalvez@gmail.com>
2025-12-10 17:20:19 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,32 @@
 # terraform/outputs.tf
+###############################################################
+# Terraform Outputs – OKD SNO + Infra Node
+###############################################################
 
-###############################################################
-# Terraform Outputs – OKD SNO
-###############################################################
+#############################
+# SNO (Single Node OpenShift)
+#############################
 
 output "sno_ip" {
-  description = "Dirección IP asignada al nodo Single Node OpenShift"
+  description = "Dirección IP del nodo SNO"
   value       = var.sno.ip
 }
 
 output "sno_hostname" {
-  description = "Hostname completo (FQDN) del nodo SNO"
+  description = "Hostname (FQDN) del nodo SNO"
   value       = var.sno.hostname
+}
+
+#############################
+# Infra Node (DNS + NTP)
+#############################
+
+output "infra_ip" {
+  description = "Dirección IP del nodo Infra (DNS + NTP)"
+  value       = var.infra.ip
+}
+
+output "infra_hostname" {
+  description = "Hostname (FQDN) del nodo Infra"
+  value       = var.infra.hostname
 }
